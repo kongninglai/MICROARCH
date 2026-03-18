52f3f879361581fc0a503d8855d04ed9a6722edd
kongninglai <kongning.lai@gmail.com>
2026-03-18 16:13:55 -0500

    fixed some muxes

--- project/hdl/PRF/gpr_shifter.v
+++ project/hdl/PRF/gpr_shifter.v
@@ -7,7 +7,7 @@ module gpr_shifter (
     wire [31:0] read_low8, read_high8, read8, read16;
     assign read_low8 = {24'b0, read_data[7:0]};
     assign read_high8 = {24'b0, read_data[15:8]};
-    mux2$ mux2_read8[31:0](read8, read_low8, read_high8, read_idx[2]);
+    mux2_32 mux2_read8(read8, read_low8, read_high8, read_idx[2]);
     assign read16 = {16'b0, read_data[15:0]};
-    mux4$ mux4_readdata[31:0](read_out, read8, read16, read_data, read_data, datasize[0], datasize[1]);
+    mux4_32 mux4_readdata(read_out, read8, read16, read_data, read_data, datasize[0], datasize[1]);
 endmodule
--- project/hdl/local-lib/muxes/mux8_64.v
+++ project/hdl/local-lib/muxes/mux8_64.v
@@ -14,8 +14,10 @@ module mux8_64(
 ); 
 
     wire [63:0] mux4_64_out0, mux4_64_out1;
-
-    mux4_64 mux4_64_0(in0, in1, in2, in3, s0, s1, mux4_64_out0);
-    mux4_64 mux4_64_1(in4, in5, in6, in7, s0, s1, mux4_64_out1);
+    wire s0_buffered, s1_buffered;
+    bufferH16$ buffer16_s0(s0_buffered, s0);
+    bufferH16$ buffer16_s1(s1_buffered, s1);
+    mux4_64 mux4_64_0(in0, in1, in2, in3, s0_buffered, s1_buffered, mux4_64_out0);
+    mux4_64 mux4_64_1(in4, in5, in6, in7, s0_buffered, s1_buffered, mux4_64_out1);
     mux2_64 mux2_64_out(mux4_64_out0, mux4_64_out1, s2, out);
 endmodule
