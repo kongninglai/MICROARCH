aa113ca82df0ca3f3ab944ad49c12eb42fe6797e
rhit-ranadeva <ranadeva@rose-hulman.edu>
2026-03-27 15:03:05 -0500

    Update reg tests to print where missing FAILURES/SUCCESSES statements are

--- project/scripts/regression/reg_test_run.sh
+++ project/scripts/regression/reg_test_run.sh
@@ -45,8 +45,15 @@ run_one_sim() {
       exit 0
     fi
 
-    failures=$(grep -Eo 'FAILURES *= *[0-9]+' sim.log | awk '{print $NF}')
-    successes=$(grep -Eo 'SUCCESSES *= *[0-9]+' sim.log | awk '{print $NF}')
+    if ! failures=$(grep -Eo 'FAILURES *= *[0-9]+' sim.log | awk '{print $NF}'); then
+      echo "No failure message found"
+      failures=""
+    fi
+
+    if ! successes=$(grep -Eo 'SUCCESSES *= *[0-9]+' sim.log | awk '{print $NF}'); then
+      echo "No success message found"
+      successes=""
+    fi
 
     failures=${failures:-999}
     successes=${successes:-0}
