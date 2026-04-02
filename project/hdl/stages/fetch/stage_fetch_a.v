7b3416275971f272a6320c25e8aafbbef99ade34
aleenakhatum <aleenak@gmail.com>
2026-04-01 20:41:24 -0500

    intantiated stage_Fetch

---
+++ project/hdl/stages/fetch/stage_fetch_a.v
@@ -0,0 +1,48 @@
+module stage_fetch (
+    input wire clk,
+    input wire rst_bar,
+
+    // TLB I/O
+    output wire [19:0] ITLB_VPN,
+    inout wire [2:0]  ITLB_PFN_OUT, 
+    input wire ITLB_PAGE_FAULT_OUT,
+
+    // Cache I/o
+    inout wire ICACHE_VALID,
+    inout wire [127:0] ICACHE_HIT_DATA,
+    output wire [11:0] F_PAGE_OFFSET,
+
+    // Pipeline Inputs 
+    input wire from_fetch_buffer_write_enable,
+    input wire [31:0] from_de_bp_target,
+    input wire from_de_eip_redirection,
+    input wire [15:0] from_rr_cs,
+    input wire [31:0] from_ex_eip_target,
+    input wire from_ex_flush,
+    input wire from_wb_flush
+);
+
+    wire [31:0] ic_addr;
+
+    // Fetch Pointer Logic
+    fetch_pointer FETCH_POINTER(
+        .clk(clk), 
+        .rst_bar(rst_n), 
+        .shft_reg_we(from_fetch_buffer_write_enable), 
+        .from_f_cl_ld(from_fetch_buffer_write_enable), 
+        .from_ex_ld_cs(1'b0), 
+        .from_ex_cs_reg(from_rr_cs),
+        .from_de_take_branch(from_de_eip_redirection),
+        .from_ex_flush(from_ex_flush), 
+        .bp_eip_target(from_de_bp_target),
+        .ex_eip_target(from_ex_eip_target),
+
+        .ic_addr(ic_addr)
+    );
+
+
+    // 32-bit address = 20-bit VPN + 12-bit Page Offset 
+    assign ITLB_VPN = ic_addr[31:12]; 
+    assign F_PAGE_OFFSET = ic_addr[11:0];  
+
+endmodule
