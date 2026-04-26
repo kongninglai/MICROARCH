dc1a03b059808d772f711163453223f9acf8335a
rhit-ranadeva <ranadeva@rose-hulman.edu>
2026-04-26 11:33:28 -0500

    Tested bp cp with evil .lst testcase. Works with 0.8ns slack. Runs at 9.3ns

--- project/hdl/stages/decoder/br_target.v
+++ project/hdl/stages/decoder/br_target.v
@@ -8,5 +8,17 @@ module br_target(
     output wire [31:0] bp_eip_target
 );  
     assign hit = 0;
-    PA_32b TARGET_rel32_adder(.in0(i_eip), .in1(imm), .s(bp_eip_target));
+    wire [31:0] bp_eip_target_ungated;
+    assign bp_eip_target[31:20] = 12'd0;
+    assign bp_eip_target[15:0] = bp_eip_target_ungated[15:0];
+    
+    wire [3:0] dummy;
+    mux2_8$ mux2_8$_clr_top_16
+    (
+      {dummy, bp_eip_target[19:16]},
+      {4'd0, bp_eip_target_ungated[19:16]},
+      {8'd0},
+      op_size_overload
+    );
+    PA_32b TARGET_rel32_adder(.in0(i_eip), .in1(imm), .s(bp_eip_target_ungated));
 endmodule
