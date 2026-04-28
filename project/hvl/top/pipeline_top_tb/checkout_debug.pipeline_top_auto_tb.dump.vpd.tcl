87c1f9f408c8ae1a83f7523b1913d2c46ee22bf3
rhit-ranadeva <ranadeva@rose-hulman.edu>
2026-04-28 15:03:20 -0500

    fix illegal double buffer in TLB. add checkout debug waveform config. Add 3 testcases. Found SS/DS testcase which failed. Emily has fixed on other branch

---
+++ project/hvl/top/pipeline_top_tb/checkout_debug.pipeline_top_auto_tb.dump.vpd.tcl
@@ -0,0 +1,419 @@
+# Begin_DVE_Session_Save_Info
+# DVE full session
+# Saved on Tue Apr 28 14:30:24 2026
+# Designs open: 1
+#   V1: pipeline_top_auto_tb.dump.vpd
+# Toplevel windows open: 2
+# 	TopLevel.1
+# 	TopLevel.2
+#   Source.1: pipeline_top_auto_tb
+#   Wave.1: 25 signals
+#   Group count = 7
+#   Group RR signal count = 3
+#   Group F signal count = 7
+#   Group DE signal count = 4
+#   Group AG signal count = 2
+#   Group MEM signal count = 2
+#   Group EX signal count = 3
+#   Group WB signal count = 4
+# End_DVE_Session_Save_Info
+
+# DVE version: T-2022.06_Full64
+# DVE build date: May 31 2022 20:53:03
+
+
+#<Session mode="Full" path="/home/ecelrc/students/var2427/MICROARCH/project/hvl/top/pipeline_top_tb/checkout_debug.pipeline_top_auto_tb.dump.vpd.tcl" type="Debug">
+
+gui_set_loading_session_type Post
+gui_continuetime_set
+
+# Close design
+if { [gui_sim_state -check active] } {
+    gui_sim_terminate
+}
+gui_close_db -all
+gui_expr_clear_all
+
+# Close all windows
+gui_close_window -type Console
+gui_close_window -type Wave
+gui_close_window -type Source
+gui_close_window -type Schematic
+gui_close_window -type Data
+gui_close_window -type DriverLoad
+gui_close_window -type List
+gui_close_window -type Memory
+gui_close_window -type HSPane
+gui_close_window -type DLPane
+gui_close_window -type Assertion
+gui_close_window -type CovHier
+gui_close_window -type CoverageTable
+gui_close_window -type CoverageMap
+gui_close_window -type CovDetail
+gui_close_window -type Local
+gui_close_window -type Stack
+gui_close_window -type Watch
+gui_close_window -type Group
+gui_close_window -type Transaction
+
+
+
+# Application preferences
+gui_set_pref_value -key app_default_font -value {Helvetica,10,-1,5,50,0,0,0,0,0}
+gui_src_preferences -tabstop 8 -maxbits 24 -windownumber 1
+#<WindowLayout>
+
+# DVE top-level session
+
+
+# Create and position top-level window: TopLevel.1
+
+if {![gui_exist_window -window TopLevel.1]} {
+    set TopLevel.1 [ gui_create_window -type TopLevel \
+       -icon $::env(DVE)/auxx/gui/images/toolbars/dvewin.xpm] 
+} else { 
+    set TopLevel.1 TopLevel.1
+}
+gui_show_window -window ${TopLevel.1} -show_state maximized -rect {{1920 23} {3839 1031}}
+
+# ToolBar settings
+gui_set_toolbar_attributes -toolbar {TimeOperations} -dock_state top
+gui_set_toolbar_attributes -toolbar {TimeOperations} -offset 0
+gui_show_toolbar -toolbar {TimeOperations}
+gui_hide_toolbar -toolbar {&File}
+gui_set_toolbar_attributes -toolbar {&Edit} -dock_state top
+gui_set_toolbar_attributes -toolbar {&Edit} -offset 0
+gui_show_toolbar -toolbar {&Edit}
+gui_hide_toolbar -toolbar {CopyPaste}
+gui_set_toolbar_attributes -toolbar {&Trace} -dock_state top
+gui_set_toolbar_attributes -toolbar {&Trace} -offset 0
+gui_show_toolbar -toolbar {&Trace}
+gui_hide_toolbar -toolbar {TraceInstance}
+gui_hide_toolbar -toolbar {BackTrace}
+gui_set_toolbar_attributes -toolbar {&Scope} -dock_state top
+gui_set_toolbar_attributes -toolbar {&Scope} -offset 0
+gui_show_toolbar -toolbar {&Scope}
+gui_set_toolbar_attributes -toolbar {&Window} -dock_state top
+gui_set_toolbar_attributes -toolbar {&Window} -offset 0
+gui_show_toolbar -toolbar {&Window}
+gui_set_toolbar_attributes -toolbar {Signal} -dock_state top
+gui_set_toolbar_attributes -toolbar {Signal} -offset 0
+gui_show_toolbar -toolbar {Signal}
+gui_set_toolbar_attributes -toolbar {Zoom} -dock_state top
+gui_set_toolbar_attributes -toolbar {Zoom} -offset 0
+gui_show_toolbar -toolbar {Zoom}
+gui_set_toolbar_attributes -toolbar {Zoom And Pan History} -dock_state top
+gui_set_toolbar_attributes -toolbar {Zoom And Pan History} -offset 0
+gui_show_toolbar -toolbar {Zoom And Pan History}
+gui_set_toolbar_attributes -toolbar {Grid} -dock_state top
+gui_set_toolbar_attributes -toolbar {Grid} -offset 0
+gui_show_toolbar -toolbar {Grid}
+gui_hide_toolbar -toolbar {Simulator}
+gui_hide_toolbar -toolbar {Interactive Rewind}
+gui_hide_toolbar -toolbar {Testbench}
+
+# End ToolBar settings
+
+# Docked window settings
+set HSPane.1 [gui_create_window -type HSPane -parent ${TopLevel.1} -dock_state left -dock_on_new_line true -dock_extent 472]
+catch { set Hier.1 [gui_share_window -id ${HSPane.1} -type Hier] }
+gui_set_window_pref_key -window ${HSPane.1} -key dock_width -value_type integer -value 472
+gui_set_window_pref_key -window ${HSPane.1} -key dock_height -value_type integer -value -1
+gui_set_window_pref_key -window ${HSPane.1} -key dock_offset -value_type integer -value 0
+gui_update_layout -id ${HSPane.1} {{left 0} {top 0} {width 471} {height 754} {dock_state left} {dock_on_new_line true} {child_hier_colhier 433} {child_hier_coltype 108} {child_hier_colpd 0} {child_hier_col1 0} {child_hier_col2 1} {child_hier_col3 -1}}
+set DLPane.1 [gui_create_window -type DLPane -parent ${TopLevel.1} -dock_state left -dock_on_new_line true -dock_extent 457]
+catch { set Data.1 [gui_share_window -id ${DLPane.1} -type Data] }
+gui_set_window_pref_key -window ${DLPane.1} -key dock_width -value_type integer -value 457
+gui_set_window_pref_key -window ${DLPane.1} -key dock_height -value_type integer -value 764
+gui_set_window_pref_key -window ${DLPane.1} -key dock_offset -value_type integer -value 0
+gui_update_layout -id ${DLPane.1} {{left 0} {top 0} {width 456} {height 754} {dock_state left} {dock_on_new_line true} {child_data_colvariable 247} {child_data_colvalue 105} {child_data_coltype 121} {child_data_col1 0} {child_data_col2 1} {child_data_col3 2}}
+set Console.1 [gui_create_window -type Console -parent ${TopLevel.1} -dock_state bottom -dock_on_new_line true -dock_extent 169]
+gui_set_window_pref_key -window ${Console.1} -key dock_width -value_type integer -value -1
+gui_set_window_pref_key -window ${Console.1} -key dock_height -value_type integer -value 169
+gui_set_window_pref_key -window ${Console.1} -key dock_offset -value_type integer -value 0
+gui_update_layout -id ${Console.1} {{left 0} {top 0} {width 271} {height 179} {dock_state bottom} {dock_on_new_line true}}
+set DriverLoad.1 [gui_create_window -type DriverLoad -parent ${TopLevel.1} -dock_state bottom -dock_on_new_line false -dock_extent 180]
+gui_set_window_pref_key -window ${DriverLoad.1} -key dock_width -value_type integer -value 150
+gui_set_window_pref_key -window ${DriverLoad.1} -key dock_height -value_type integer -value 180
+gui_set_window_pref_key -window ${DriverLoad.1} -key dock_offset -value_type integer -value 0
+gui_update_layout -id ${DriverLoad.1} {{left 0} {top 0} {width 1647} {height 179} {dock_state bottom} {dock_on_new_line false}}
+#### Start - Readjusting docked view's offset / size
+set dockAreaList { top left right bottom }
+foreach dockArea $dockAreaList {
+  set viewList [gui_ekki_get_window_ids -active_parent -dock_area $dockArea]
+  foreach view $viewList {
+      if {[lsearch -exact [gui_get_window_pref_keys -window $view] dock_width] != -1} {
+        set dockWidth [gui_get_window_pref_value -window $view -key dock_width]
+        set dockHeight [gui_get_window_pref_value -window $view -key dock_height]
+        set offset [gui_get_window_pref_value -window $view -key dock_offset]
+        if { [string equal "top" $dockArea] || [string equal "bottom" $dockArea]} {
+          gui_set_window_attributes -window $view -dock_offset $offset -width $dockWidth
+        } else {
+          gui_set_window_attributes -window $view -dock_offset $offset -height $dockHeight
+        }
+      }
+  }
+}
+#### End - Readjusting docked view's offset / size
+gui_sync_global -id ${TopLevel.1} -option true
+
+# MDI window settings
+set Source.1 [gui_create_window -type {Source}  -parent ${TopLevel.1}]
+gui_show_window -window ${Source.1} -show_state maximized
+gui_update_layout -id ${Source.1} {{show_state maximized} {dock_state undocked} {dock_on_new_line false}}
+
+# End MDI window settings
+
+
+# Create and position top-level window: TopLevel.2
+
+if {![gui_exist_window -window TopLevel.2]} {
+    set TopLevel.2 [ gui_create_window -type TopLevel \
+       -icon $::env(DVE)/auxx/gui/images/toolbars/dvewin.xpm] 
+} else { 
+    set TopLevel.2 TopLevel.2
+}
+gui_show_window -window ${TopLevel.2} -show_state maximized -rect {{1920 23} {3839 1031}}
+
+# ToolBar settings
+gui_set_toolbar_attributes -toolbar {TimeOperations} -dock_state top
+gui_set_toolbar_attributes -toolbar {TimeOperations} -offset 0
+gui_show_toolbar -toolbar {TimeOperations}
+gui_hide_toolbar -toolbar {&File}
+gui_set_toolbar_attributes -toolbar {&Edit} -dock_state top
+gui_set_toolbar_attributes -toolbar {&Edit} -offset 0
+gui_show_toolbar -toolbar {&Edit}
+gui_hide_toolbar -toolbar {CopyPaste}
+gui_set_toolbar_attributes -toolbar {&Trace} -dock_state top
+gui_set_toolbar_attributes -toolbar {&Trace} -offset 0
+gui_show_toolbar -toolbar {&Trace}
+gui_hide_toolbar -toolbar {TraceInstance}
+gui_hide_toolbar -toolbar {BackTrace}
+gui_set_toolbar_attributes -toolbar {&Scope} -dock_state top
+gui_set_toolbar_attributes -toolbar {&Scope} -offset 0
+gui_show_toolbar -toolbar {&Scope}
+gui_set_toolbar_attributes -toolbar {&Window} -dock_state top
+gui_set_toolbar_attributes -toolbar {&Window} -offset 0
+gui_show_toolbar -toolbar {&Window}
+gui_set_toolbar_attributes -toolbar {Signal} -dock_state top
+gui_set_toolbar_attributes -toolbar {Signal} -offset 0
+gui_show_toolbar -toolbar {Signal}
+gui_set_toolbar_attributes -toolbar {Zoom} -dock_state top
+gui_set_toolbar_attributes -toolbar {Zoom} -offset 0
+gui_show_toolbar -toolbar {Zoom}
+gui_set_toolbar_attributes -toolbar {Zoom And Pan History} -dock_state top
+gui_set_toolbar_attributes -toolbar {Zoom And Pan History} -offset 0
+gui_show_toolbar -toolbar {Zoom And Pan History}
+gui_set_toolbar_attributes -toolbar {Grid} -dock_state top
+gui_set_toolbar_attributes -toolbar {Grid} -offset 0
+gui_show_toolbar -toolbar {Grid}
+gui_hide_toolbar -toolbar {Simulator}
+gui_hide_toolbar -toolbar {Interactive Rewind}
+gui_set_toolbar_attributes -toolbar {Testbench} -dock_state top
+gui_set_toolbar_attributes -toolbar {Testbench} -offset 0
+gui_show_toolbar -toolbar {Testbench}
+
+# End ToolBar settings
+
+# Docked window settings
+gui_sync_global -id ${TopLevel.2} -option true
+
+# MDI window settings
+set Wave.1 [gui_create_window -type {Wave}  -parent ${TopLevel.2}]
+gui_show_window -window ${Wave.1} -show_state maximized
+gui_update_layout -id ${Wave.1} {{show_state maximized} {dock_state undocked} {dock_on_new_line false} {child_wave_left 557} {child_wave_right 1357} {child_wave_colname 276} {child_wave_colvalue 276} {child_wave_col1 0} {child_wave_col2 1}}
+
+# End MDI window settings
+
+gui_set_env TOPLEVELS::TARGET_FRAME(Source) ${TopLevel.1}
+gui_set_env TOPLEVELS::TARGET_FRAME(Schematic) ${TopLevel.1}
+gui_set_env TOPLEVELS::TARGET_FRAME(PathSchematic) ${TopLevel.1}
+gui_set_env TOPLEVELS::TARGET_FRAME(Wave) none
+gui_set_env TOPLEVELS::TARGET_FRAME(List) none
+gui_set_env TOPLEVELS::TARGET_FRAME(Memory) ${TopLevel.1}
+gui_set_env TOPLEVELS::TARGET_FRAME(DriverLoad) none
+gui_update_statusbar_target_frame ${TopLevel.1}
+gui_update_statusbar_target_frame ${TopLevel.2}
+
+#</WindowLayout>
+
+#<Database>
+
+# DVE Open design session: 
+
+if { ![gui_is_db_opened -db {pipeline_top_auto_tb.dump.vpd}] } {
+	gui_open_db -design V1 -file pipeline_top_auto_tb.dump.vpd -nosource
+}
+gui_set_precision 1ps
+gui_set_time_units 1ns
+#</Database>
+
+# DVE Global setting session: 
+
+
+# Global: Bus
+
+# Global: Expressions
+
+# Global: Signal Time Shift
+
+# Global: Signal Compare
+
+# Global: Signal Groups
+gui_load_child_values {pipeline_top_auto_tb.FRONTEND_TOP.ENABLE_INSTRQ.FETCHBUFF_DECODESTAGE_DEPR}
+gui_load_child_values {pipeline_top_auto_tb.FRONTEND_TOP.STAGE_FETCH_FRONT_HALF}
+gui_load_child_values {pipeline_top_auto_tb.FRONTEND_TOP.ENABLE_INSTRQ.FETCHBUFF_DECODESTAGE_DEPR.STAGE_DECODE}
+
+
+set _session_group_8 RR
+gui_sg_create "$_session_group_8"
+set RR "$_session_group_8"
+
+gui_sg_addsignal -group "$_session_group_8" { pipeline_top_auto_tb.to_rr_opcode pipeline_top_auto_tb.to_rr_oeip pipeline_top_auto_tb.to_rr_valid }
+
+set _session_group_9 F
+gui_sg_create "$_session_group_9"
+set F "$_session_group_9"
+
+gui_sg_addsignal -group "$_session_group_9" { pipeline_top_auto_tb.clk pipeline_top_auto_tb.FRONTEND_TOP.STAGE_FETCH_FRONT_HALF.ic_addr pipeline_top_auto_tb.FRONTEND_TOP.STAGE_FETCH_FRONT_HALF.FETCH_POINTER.shft_reg_we pipeline_top_auto_tb.FRONTEND_TOP.ENABLE_INSTRQ.FETCHBUFF_DECODESTAGE_DEPR.from_f_cache_line pipeline_top_auto_tb.FRONTEND_TOP.ENABLE_INSTRQ.FETCHBUFF_DECODESTAGE_DEPR.ICACHE_VALID pipeline_top_auto_tb.FRONTEND_TOP.ENABLE_INSTRQ.FETCHBUFF_DECODESTAGE_DEPR.FETCH_BUFF.tail_ptr pipeline_top_auto_tb.FRONTEND_TOP.ENABLE_INSTRQ.FETCHBUFF_DECODESTAGE_DEPR.FETCH_BUFF.true_consume }
+
+set _session_group_10 DE
+gui_sg_create "$_session_group_10"
+set DE "$_session_group_10"
+
+gui_sg_addsignal -group "$_session_group_10" { pipeline_top_auto_tb.FRONTEND_TOP.ENABLE_INSTRQ.FETCHBUFF_DECODESTAGE_DEPR.STAGE_DECODE.cache_line pipeline_top_auto_tb.FRONTEND_TOP.ENABLE_INSTRQ.FETCHBUFF_DECODESTAGE_DEPR.STAGE_DECODE.instr_length pipeline_top_auto_tb.FRONTEND_TOP.ENABLE_INSTRQ.FETCHBUFF_DECODESTAGE_DEPR.PR_DE_RR.entry_count pipeline_top_auto_tb.FRONTEND_TOP.ENABLE_INSTRQ.FETCHBUFF_DECODESTAGE_DEPR.PR_DE_RR.from_de_pr_valid }
+
+set _session_group_11 AG
+gui_sg_create "$_session_group_11"
+set AG "$_session_group_11"
+
+gui_sg_addsignal -group "$_session_group_11" { pipeline_top_auto_tb.dut.to_ag_oeip pipeline_top_auto_tb.dut.to_ag_valid }
+
+set _session_group_12 MEM
+gui_sg_create "$_session_group_12"
+set MEM "$_session_group_12"
+
+gui_sg_addsignal -group "$_session_group_12" { pipeline_top_auto_tb.dut.to_mem_oeip pipeline_top_auto_tb.dut.to_mem_valid }
+
+set _session_group_13 EX
+gui_sg_create "$_session_group_13"
+set EX "$_session_group_13"
+
+gui_sg_addsignal -group "$_session_group_13" { pipeline_top_auto_tb.dut.to_ex_oeip pipeline_top_auto_tb.dut.to_ex_valid pipeline_top_auto_tb.dut.from_ex_flush }
+
+set _session_group_14 WB
+gui_sg_create "$_session_group_14"
+set WB "$_session_group_14"
+
+gui_sg_addsignal -group "$_session_group_14" { pipeline_top_auto_tb.dut.from_wb_flush pipeline_top_auto_tb.dut.to_wb_oeip pipeline_top_auto_tb.dut.to_wb_valid pipeline_top_auto_tb.dut.to_wb_exception }
+
+# Global: Highlighting
+
+# Global: Stack
+gui_change_stack_mode -mode list
+
+# Post database loading setting...
+
+# Restore C1 time
+gui_set_time -C1_only 2066.794
+
+
+
+# Save global setting...
+
+# Wave/List view global setting
+gui_cov_show_value -switch false
+
+# Close all empty TopLevel windows
+foreach __top [gui_ekki_get_window_ids -type TopLevel] {
+    if { [llength [gui_ekki_get_window_ids -parent $__top]] == 0} {
+        gui_close_window -window $__top
+    }
+}
+gui_set_loading_session_type noSession
+# DVE View/pane content session: 
+
+
+# Hier 'Hier.1'
+gui_show_window -window ${Hier.1}
+gui_list_set_filter -id ${Hier.1} -list { {Package 1} {All 0} {Process 1} {VirtPowSwitch 0} {UnnamedProcess 1} {UDP 0} {Function 1} {Block 1} {SrsnAndSpaCell 0} {OVA Unit 1} {LeafScCell 1} {LeafVlgCell 1} {Interface 1} {LeafVhdCell 1} {$unit 1} {NamedBlock 1} {Task 1} {VlgPackage 1} {ClassDef 1} {VirtIsoCell 0} }
+gui_list_set_filter -id ${Hier.1} -text {*}
+gui_hier_list_init -id ${Hier.1}
+gui_change_design -id ${Hier.1} -design V1
+catch {gui_list_expand -id ${Hier.1} pipeline_top_auto_tb}
+catch {gui_list_expand -id ${Hier.1} pipeline_top_auto_tb.dut}
+catch {gui_list_select -id ${Hier.1} {pipeline_top_auto_tb.dut.inst_ag}}
+gui_view_scroll -id ${Hier.1} -vertical -set 316
+gui_view_scroll -id ${Hier.1} -horizontal -set 0
+
+# Data 'Data.1'
+gui_list_set_filter -id ${Data.1} -list { {Buffer 1} {Input 1} {Others 1} {Linkage 1} {Output 1} {LowPower 1} {Parameter 1} {All 1} {Aggregate 1} {LibBaseMember 1} {Event 1} {Assertion 1} {Constant 1} {Interface 1} {BaseMembers 1} {Signal 1} {$unit 1} {Inout 1} {Variable 1} }
+gui_list_set_filter -id ${Data.1} -text {*}
+gui_list_show_data -id ${Data.1} {pipeline_top_auto_tb.dut.inst_ag}
+gui_show_window -window ${Data.1}
+catch { gui_list_select -id ${Data.1} {pipeline_top_auto_tb.dut.inst_ag.to_ag_base1 pipeline_top_auto_tb.dut.inst_ag.to_ag_index1 pipeline_top_auto_tb.dut.inst_ag.to_ag_disp }}
+gui_view_scroll -id ${Data.1} -vertical -set 0
+gui_view_scroll -id ${Data.1} -horizontal -set 0
+gui_view_scroll -id ${Hier.1} -vertical -set 316
+gui_view_scroll -id ${Hier.1} -horizontal -set 0
+
+# Source 'Source.1'
+gui_src_value_annotate -id ${Source.1} -switch false
+gui_set_env TOGGLE::VALUEANNOTATE 0
+gui_open_source -id ${Source.1}  -replace -active pipeline_top_auto_tb ../pipeline_top_auto_tb.v
+gui_view_scroll -id ${Source.1} -vertical -set 13095
+gui_src_set_reusable -id ${Source.1}
+
+# View 'Wave.1'
+gui_wv_sync -id ${Wave.1} -switch false
+set groupExD [gui_get_pref_value -category Wave -key exclusiveSG]
+gui_set_pref_value -category Wave -key exclusiveSG -value {false}
+set origWaveHeight [gui_get_pref_value -category Wave -key waveRowHeight]
+gui_list_set_height -id Wave -height 25
+set origGroupCreationState [gui_list_create_group_when_add -wave]
+gui_list_create_group_when_add -wave -disable
+gui_marker_set_ref -id ${Wave.1}  C1
+gui_wv_zoom_timerange -id ${Wave.1} 1944.746 2080.615
+gui_list_add_group -id ${Wave.1} -after {New Group} {F}
+gui_list_add_group -id ${Wave.1} -after {New Group} {DE}
+gui_list_add_group -id ${Wave.1} -after {New Group} {RR}
+gui_list_add_group -id ${Wave.1} -after {New Group} {AG}
+gui_list_add_group -id ${Wave.1} -after {New Group} {MEM}
+gui_list_add_group -id ${Wave.1} -after {New Group} {EX}
+gui_list_add_group -id ${Wave.1} -after {New Group} {WB}
+gui_seek_criteria -id ${Wave.1} {Any Edge}
+
+
+
+gui_set_env TOGGLE::DEFAULT_WAVE_WINDOW ${Wave.1}
+gui_set_pref_value -category Wave -key exclusiveSG -value $groupExD
+gui_list_set_height -id Wave -height $origWaveHeight
+if {$origGroupCreationState} {
+	gui_list_create_group_when_add -wave -enable
+}
+if { $groupExD } {
+ gui_msg_report -code DVWW028
+}
+gui_list_set_filter -id ${Wave.1} -list { {Buffer 1} {Input 1} {Others 1} {Linkage 1} {Output 1} {Parameter 1} {All 1} {Aggregate 1} {LibBaseMember 1} {Event 1} {Assertion 1} {Constant 1} {Interface 1} {BaseMembers 1} {Signal 1} {$unit 1} {Inout 1} {Variable 1} }
+gui_list_set_filter -id ${Wave.1} -text {*}
+gui_list_set_insertion_bar  -id ${Wave.1} -group AG  -item pipeline_top_auto_tb.dut.to_ag_valid -position below
+
+gui_marker_move -id ${Wave.1} {C1} 2066.794
+gui_view_scroll -id ${Wave.1} -vertical -set 1
+gui_show_grid -id ${Wave.1} -enable false
+
+# DriverLoad 'DriverLoad.1'
+gui_get_drivers -session -id ${DriverLoad.1} -signal pipeline_top_auto_tb.clk -time 0 -starttime 0
+# Restore toplevel window zorder
+# The toplevel window could be closed if it has no view/pane
+if {[gui_exist_window -window ${TopLevel.1}]} {
+	gui_set_active_window -window ${TopLevel.1}
+	gui_set_active_window -window ${Source.1}
+	gui_set_active_window -window ${DLPane.1}
+}
+if {[gui_exist_window -window ${TopLevel.2}]} {
+	gui_set_active_window -window ${TopLevel.2}
+	gui_set_active_window -window ${Wave.1}
+}
+#</Session>
+
